8703b0d388d66ecc1ff787780c50a8fc5cd7596f
Sam Kautz <50638220+Web-Dev-Jr@users.noreply.github.com>
2023-07-25 21:38:07 -0500

    Update Search.js in clientapp

--- TwitterShowcase/ClientApp/src/components/Search.js
+++ TwitterShowcase/ClientApp/src/components/Search.js
@@ -2,16 +2,34 @@ import React, { useState } from 'react';
 
 const SearchPage = () => {
     const [twitterHandle, setTwitterHandle] = useState('');
+    const [tweets, setTweets] = useState([]);
 
-    const handleInputChange = (event) => {
-        setTwitterHandle(event.target.value);
+    const handleInputChange = (e) => {
+        setTwitterHandle(e.target.value);
     };
 
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
-
-        console.log('Twitter Handle:', twitterHandle);
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        fetchTweets(twitterHandle);
     };
+
+    const fetchTweets = async (twitterHandle) => {
+        try {
+            const response = await fetch(`/api/tweets/${twitterHandle}`);
+            if (response.ok) {
+                const content = await response.text();
+                console.log('Response Content:', content); // Logging response
+                const data = JSON.parse(content);
+                setTweets(data.data);
+            } else {
+                console.error('Failed to fetch tweets:', response.statusText);
+            }
+        } catch (err) {
+            console.error('An error occurred while fetching tweets:', err);
+        }
+    };
+
+
 
     return (
         <div className="container mt-5">
